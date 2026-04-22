f09911c65919d3b785a6bc23a44449277bb66eca
vivek-prometteur <vivek.v@prometteur.in>
2026-04-22 11:02:46 +0530

    setup product create and get in postman

--- backend/middleware/permission.middleware.js
+++ backend/middleware/permission.middleware.js
@@ -3,13 +3,30 @@ import User from "../models/user.model.js";
 export const checkPermission = (requiredPermission) => {
   return async (req, res, next) => {
     try {
+      //  GET FROM HEADER
+      const userId = req.headers["x-user-id"];
+
+      if (!userId) {
+        return res.status(401).json({ message: "No user id provided" });
+      }
+
+      //  attach to req.user
+      req.user = { _id: userId };
+
+      //  FIXED HERE
       const user = await User.findById(req.user._id).populate({
         path: "role",
         populate: { path: "permissions" },
       });
-      if (!user || !user.role) {
-        return res.status(403).json({ message: "Access denied" });
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
       }
+
+      if (!user.role) {
+        return res.status(403).json({ message: "Role not assigned" });
+      }
+
       const userPermissions = user.role.permissions.map((p) => p.name);
 
       if (!userPermissions.includes(requiredPermission)) {
--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -2,9 +2,12 @@
 import User from "../models/user.model.js";
 
 export const assignRoleToUser = async (userId, roleId) => {
-  return await User.findByIdAndUpdate(
-    userId,
-    { role: roleId },
-    { new: true }
-  );
+  return await User.findByIdAndUpdate(userId, { role: roleId }, { new: true });
+};
+
+export const getUsers = async () => {
+  return await User.find().populate({
+    path: "role",
+    populate: { path: "permissions" },
+  });
 };
