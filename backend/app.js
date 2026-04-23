e2fea32cb3f4a5ae907e98c9ad34b5e8e32cfec5
vivek-prometteur <vivek.v@prometteur.in>
2026-04-23 14:32:37 +0530

    add cors

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const { seedPermissions } = require("./seeds/permission.seed");
 const errorHandler = require("./middleware/error.middleware").default;
 
 const app = express();
 connectDB();
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend URL
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth.route"));
 app.use("/api/roles", require("./routes/role.route"));
-app.use("/api/products",require("./routes/product.route"));
-app.use("/api/users",require("./routes/user.route"))
-app.use("/api/orders",require("./routes/order.route"))
+app.use("/api/products", require("./routes/product.route"));
+app.use("/api/users", require("./routes/user.route"));
+app.use("/api/orders", require("./routes/order.route"));
 // seedPermissions()
 app.use(errorHandler);
 
