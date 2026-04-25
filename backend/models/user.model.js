9b1829fe12dd8868da7efe9d3e7e6c7c4aa12334
vivek-prometteur <vivek.v@prometteur.in>
2026-04-25 14:34:01 +0530

    add block user field in user table

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -23,6 +23,10 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true },
 );
