449208f89e998dc03c3a01898bb405e8ec75bb67
vivek-prometteur <vivek.v@prometteur.in>
2026-05-14 16:17:21 +0530

    fix user model issue

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -36,11 +36,10 @@ const userSchema = new mongoose.Schema(
   { timestamps: true },
 );
 
-userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 module.exports = mongoose.model("User", userSchema);
